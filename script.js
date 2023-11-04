ea0250e9c681d55ea2c9c4523e2ebc0c3416a00e
Elion Abdyli <53652464+elion-abdyli@users.noreply.github.com>
2023-11-03 20:09:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,119 @@
-const board = ChessBoard('board', {
-  position: 'start',
-  draggable: true,
-  onDragStart,
-  onDrop,
-  onSnapEnd
+// Constants
+const numRows = 30;
+const numCols = 50;
+const speed = 200; // In milliseconds
+
+let grid = createGrid();
+let isPlaying = false;
+let intervalId;
+
+// Create the grid
+function createGrid() {
+    const grid = new Array(numRows);
+
+    for (let i = 0; i < numRows; i++) {
+        grid[i] = new Array(numCols).fill(false);
+    }
+
+    return grid;
+}
+
+// Initialize the grid with random cells
+function randomizeGrid() {
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            grid[i][j] = Math.random() < 0.3; // Adjust the probability as needed
+        }
+    }
+    renderGrid();
+}
+
+// Render the grid
+function renderGrid() {
+    const gridContainer = document.getElementById("grid-container");
+    gridContainer.innerHTML = "";
+
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.style.backgroundColor = grid[i][j] ? "black" : "white";
+            cell.addEventListener("click", () => toggleCell(i, j));
+            gridContainer.appendChild(cell);
+        }
+    }
+}
+
+// Toggle a cell on click
+function toggleCell(row, col) {
+    grid[row][col] = !grid[row][col];
+    renderGrid();
+}
+
+// Calculate the next generation
+function nextGeneration() {
+    const newGrid = createGrid();
+
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            const neighbors = countNeighbors(i, j);
+            if (grid[i][j]) {
+                newGrid[i][j] = neighbors === 2 || neighbors === 3;
+            } else {
+                newGrid[i][j] = neighbors === 3;
+            }
+        }
+    }
+
+    grid = newGrid;
+    renderGrid();
+}
+
+// Count live neighbors for a cell
+function countNeighbors(row, col) {
+    const neighbors = [
+        [-1, -1], [-1, 0], [-1, 1],
+        [0, -1],           [0, 1],
+        [1, -1], [1, 0], [1, 1]
+    ];
+
+    let count = 0;
+
+    for (const [dr, dc] of neighbors) {
+        const newRow = row + dr;
+        const newCol = col + dc;
+
+        if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols) {
+            if (grid[newRow][newCol]) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+document.getElementById("startButton").addEventListener("click", () => {
+    if (!isPlaying) {
+        isPlaying = true;
+        intervalId = setInterval(nextGeneration, speed);
+    }
 });
 
-let game = new Chess();
-let isPlayerTurn = true;
-
-document.getElementById('startBtn').addEventListener('click', () => {
-  board.position('start');
-  game = new Chess();
-  isPlayerTurn = true;
+document.getElementById("stopButton").addEventListener("click", () => {
+    if (isPlaying) {
+        isPlaying = false;
+        clearInterval(intervalId);
+    }
 });
 
-document.getElementById('resetBtn').addEventListener('click', () => {
-  board.position('start');
-  game = new Chess();
-  isPlayerTurn = true;
+document.getElementById("clearButton").addEventListener("click", () => {
+    if (isPlaying) {
+        isPlaying = false;
+        clearInterval(intervalId);
+    }
+    grid = createGrid();
+    renderGrid();
 });
 
-function onDragStart(source, piece) {
-  if (!isPlayerTurn || game.in_checkmate() || game.in_draw() || game.in_stalemate() || game.in_threefold_repetition() || game.insufficient_material()) {
-    return false;
-  }
-}
-
-function onDrop(source, target) {
-  const move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q' // promote to queen by default
-  });
-
-  removeGreySquares();
-  board.position(game.fen());
-
-  if (!move) return;
-
-  isPlayerTurn = false;
-  setTimeout(makeBestAIMove, 250);
-}
-
-function onSnapEnd() {
-  board.position(game.fen());
-}
-
-function makeBestAIMove() {
-  const bestMove = getBestMove();
-  game.ugly_move(bestMove);
-  board.position(game.fen());
-  isPlayerTurn = true;
-}
-
-function getBestMove() {
-  // In a complete project, you'd integrate a chess engine API like Stockfish here
-  // For simplicity, this example generates a random move
-  const legalMoves = game.ugly_moves();
-  const randomMove = legalMoves[Math.floor(Math.random() * legalMoves.length)];
-  return randomMove;
-}
-
-function removeGreySquares() {
-  const elements = document.querySelectorAll('.square-55d63');
-  elements.forEach((el) => {
-    el.style.background = '';
-  });
-}
+randomizeGrid(); // You can remove this line if you want to start with an empty grid
