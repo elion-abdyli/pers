13c7e32b1b41ed4d6482f1f2619fc40f1de7a1f9
Elion Abdyli <53652464+elion-abdyli@users.noreply.github.com>
2023-11-03 20:02:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const ipAddressElement = document.getElementById("ipAddress");
-
-    fetch("https://api64.ipify.org?format=json")
-        .then(response => response.json())
-        .then(data => {
-            const ipAddress = data.ip;
-            ipAddressElement.textContent = ipAddress;
-        })
-        .catch(error => {
-            ipAddressElement.textContent = "Unable to retrieve your IP address.";
-        });
+const board = ChessBoard('board', {
+  position: 'start',
+  draggable: true,
+  onDragStart,
+  onDrop,
+  onSnapEnd
 });
+
+let game = new Chess();
+let isPlayerTurn = true;
+
+document.getElementById('startBtn').addEventListener('click', () => {
+  board.position('start');
+  game = new Chess();
+  isPlayerTurn = true;
+});
+
+document.getElementById('resetBtn').addEventListener('click', () => {
+  board.position('start');
+  game = new Chess();
+  isPlayerTurn = true;
+});
+
+function onDragStart(source, piece) {
+  if (!isPlayerTurn || game.in_checkmate() || game.in_draw() || game.in_stalemate() || game.in_threefold_repetition() || game.insufficient_material()) {
+    return false;
+  }
+}
+
+function onDrop(source, target) {
+  const move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // promote to queen by default
+  });
+
+  removeGreySquares();
+  board.position(game.fen());
+
+  if (!move) return;
+
+  isPlayerTurn = false;
+  setTimeout(makeBestAIMove, 250);
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+function makeBestAIMove() {
+  const bestMove = getBestMove();
+  game.ugly_move(bestMove);
+  board.position(game.fen());
+  isPlayerTurn = true;
+}
+
+function getBestMove() {
+  // In a complete project, you'd integrate a chess engine API like Stockfish here
+  // For simplicity, this example generates a random move
+  const legalMoves = game.ugly_moves();
+  const randomMove = legalMoves[Math.floor(Math.random() * legalMoves.length)];
+  return randomMove;
+}
+
+function removeGreySquares() {
+  const elements = document.querySelectorAll('.square-55d63');
+  elements.forEach((el) => {
+    el.style.background = '';
+  });
+}
